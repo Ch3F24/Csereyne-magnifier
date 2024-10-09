83d5fd8a65f4cd30d7233909f922761ef35aa23d
Chef <zsolt.horvath@wst.hu>
2024-10-09 20:10:18 +0200

    change description and language selector and fix sharp point

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ document.addEventListener("DOMContentLoaded", () => {
         event.preventDefault();
     });
 
+    setSharpSpotPosition(image.clientWidth / 2, image.clientHeight / 2);
+    sharpSpot.style.visibility = 'visible';
     imageContent(activeIndex)
 
     imageContainer.addEventListener('touchmove', zooming);
@@ -23,6 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
         event.preventDefault(); // Prevent scrolling
         zooming(event);
     });
+    
     // imageContainer.addEventListener('touchmove', zooming);
 
     function debounce(func, wait) {
@@ -59,37 +62,47 @@ document.addEventListener("DOMContentLoaded", () => {
         toggleBlurDebounced()
 
         const { offsetX, offsetY } = getPosition(event);
+        setSharpSpotPosition(offsetX, offsetY);
+    };
 
-
+    function setSharpSpotPosition(offsetX, offsetY) {
         const left = Math.min(Math.max(0, offsetX - sharpSpotSize / 2), imageContainer.clientWidth - sharpSpotSize);
         const top = Math.min(Math.max(0, offsetY - sharpSpotSize / 2), imageContainer.clientHeight - sharpSpotSize);
-
-        // sharpSpot.style.left = `${left}px`;
-        // sharpSpot.style.top = `${top}px`;
-        // sharpSpot.style.backgroundImage = `url('${image.src}')`;
-        // // sharpSpot.style.backgroundPosition = `-${left * zoomLevel}px -${top * zoomLevel}px`;
-        // sharpSpot.style.backgroundPosition = `-${(offsetX * zoomLevel) - (sharpSpotSize / 2)}px -${(offsetY * zoomLevel) - (sharpSpotSize / 2)}px`;
-        // sharpSpot.style.backgroundSize = `${imageContainer.clientWidth * zoomLevel}px ${imageContainer.clientHeight * zoomLevel}px`;
 
         sharpSpot.style.left = `${left}px`;
         sharpSpot.style.top = `${top}px`;
         sharpSpot.style.backgroundImage = `url('${image.src}')`;
         sharpSpot.style.backgroundPosition = `-${(offsetX * zoomLevel) - (sharpSpotSize / 2)}px -${(offsetY * zoomLevel) - (sharpSpotSize / 2)}px`;
         sharpSpot.style.backgroundSize = `${imageContainer.clientWidth * zoomLevel}px ${imageContainer.clientHeight * zoomLevel}px`;
-    };
+    }
 
     slides.forEach((slide, index) => {
         slide.addEventListener('click', () => {
-    
+            sharpSpot.style.visibility = 'hidden';
             const activeSource = slides[index].dataset.image;
             activeIndex = index;
             
             if(activeSource) {
-                image.src = 'images/' + activeSource;
-                sharpSpot.style.backgroundImage = `url('${image.src}')`;
-                sharpSpot.style.left = `0px`;
-                sharpSpot.style.top = `0px`;
-                imageContent(index);
+
+                const imagePath = 'images/' + activeSource;
+                image.src = imagePath;
+                sharpSpot.style.backgroundImage = `url('${imagePath}')`;
+
+                const testImage = new Image();
+                testImage.src = imagePath;
+
+                testImage.addEventListener('load', function() {
+                    // Background image is loaded
+                    setSharpSpotPosition(image.clientWidth / 2, image.clientHeight / 2);
+                    imageContent(index);
+                    sharpSpot.style.visibility = 'visible';
+                });
+
+                // image.src = 'images/' + activeSource;
+                // sharpSpot.style.backgroundImage = `url('${image.src}')`;
+                // setSharpSpotPosition(image.clientWidth / 2, image.clientHeight / 2);
+                // imageContent(index);
+                // sharpSpot.style.display = 'block';
             }
         });
     });
